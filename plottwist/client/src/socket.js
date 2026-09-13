e1a65426b6fd939e06d1a91fab6013848c4cdb17
woshi093 <joshua.sas09@gmail.com>
2026-09-13 16:33:29 +1000

    Delete plottwist directory

--- plottwist/client/src/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-// Points at the backend from Block 1 (server/server.js).
-// Override with VITE_SERVER_URL for a deployed backend.
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
-
-export const socket = io(SERVER_URL, { autoConnect: true });
